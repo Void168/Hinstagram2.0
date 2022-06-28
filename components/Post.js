3f3d48aa8e91403785f722621d068e2105d80819
LPHung <phuhung16820@gmail.com>
2022-06-28 22:20:37 +0700

    upload image success on firebase, map the post on newfeeds

--- components/Post.js
+++ components/Post.js
@@ -6,8 +6,10 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
 } from '@heroicons/react/outline'
+import { useSession } from 'next-auth/react'
 
-function Post({ username, userImg, img, caption }) {
+function Post({ username, userImg, img, caption, id }) {
+  const { data: session } = useSession()
   return (
     <div className="bg-white my-7 border rounded-sm">
       Post
@@ -24,14 +26,16 @@ function Post({ username, userImg, img, caption }) {
       {/* {img} */}
       <img src={img} className="object-cover w-full" alt="" />
       {/* {buttons } */}
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
-          <PaperAirplaneIcon className="btn" />
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <HeartIcon className="btn" />
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="btn" />
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
-      </div>
+      )}
       {/* {caption} */}
       <div className="p-5 truncate">
         <span>{username}</span>
@@ -39,15 +43,19 @@ function Post({ username, userImg, img, caption }) {
       </div>
       {/* {comments} */}
       {/* {input box} */}
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7" />
-        <input
-          type="text"
-          className="border-none flex-1 outline-none"
-          placeholder="Nhập bình luận..."
-        />
-        <button className="font-semibold text-blue-400">Đăng bình luận</button>
-      </form>
+      {session && (
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7" />
+          <input
+            type="text"
+            className="border-none flex-1 outline-none"
+            placeholder="Nhập bình luận..."
+          />
+          <button className="font-semibold text-blue-400">
+            Đăng bình luận
+          </button>
+        </form>
+      )}
     </div>
   )
 }
--- components/Posts.js
+++ components/Posts.js
@@ -1,38 +1,35 @@
+import { onSnapshot, orderBy, query, collection } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { db } from '../firebase'
 import Post from './Post'
 
-const posts = [
-  {
-    userId: 123,
-    username: 'phuhuwnglee',
-    userImg:
-      'https://scontent.fsgn5-3.fna.fbcdn.net/v/t1.6435-9/123584179_1449709761900168_1445279572392526145_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Wo95pKXMSawAX-hm7Rk&_nc_ht=scontent.fsgn5-3.fna&oh=00_AT9XUHxdPilbzyshjiWj853rHCcyA3pRI5snhY544OGypg&oe=62E1461F',
-    img:
-      'https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/275048914_1623249294705332_8847552586335853246_n.jpg?stp=dst-jpg_s600x600&_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=vadZD2pOHJgAX83pZmy&_nc_ht=scontent.fsgn5-12.fna&oh=00_AT_BmlUFgYdiV78M8RXQzsFtf9DSpD0vstjugMXDPh93Jw&oe=62BEAF98',
-    caption: 'My love',
-  },
-  {
-    userId: 124,
-    username: 'phuhuwnglee',
-    userImg:
-      'https://scontent.fsgn5-3.fna.fbcdn.net/v/t1.6435-9/123584179_1449709761900168_1445279572392526145_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Wo95pKXMSawAX-hm7Rk&_nc_ht=scontent.fsgn5-3.fna&oh=00_AT9XUHxdPilbzyshjiWj853rHCcyA3pRI5snhY544OGypg&oe=62E1461F',
-    img:
-      'https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/275048914_1623249294705332_8847552586335853246_n.jpg?stp=dst-jpg_s600x600&_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=vadZD2pOHJgAX83pZmy&_nc_ht=scontent.fsgn5-12.fna&oh=00_AT_BmlUFgYdiV78M8RXQzsFtf9DSpD0vstjugMXDPh93Jw&oe=62BEAF98',
-    caption: 'My love',
-  },
-]
-
 function Posts() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs)
+        },
+      ),
+    [db],
+    )
+    console.log(posts)
+
   return (
     <div>
-      {/* {posts.map((post) => {
-        <Post
-          key={post.userId}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}/>
-      })} */}
-          <Post />
+      {posts.map((post) => {
+          <Post
+          id={post.id}
+          key={post.id}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
+        />
+      })}
     </div>
   )
 }
